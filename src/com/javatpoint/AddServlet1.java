b6bd6bb06e10c4e454cad12d03afd94cb592def7
Vatsalya Mishra <vatsalyamishra93@gmail.com>
2018-09-20 11:08:33 +0530

    first commit

---
+++ src/com/javatpoint/AddServlet1.java
@@ -0,0 +1,39 @@
+package com.javatpoint;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/AddServlet1")
+public class AddServlet1 extends HttpServlet {
+	
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+
+	public void destroy() {
+		
+	}
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
